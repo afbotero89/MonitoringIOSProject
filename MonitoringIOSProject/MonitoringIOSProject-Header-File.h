856ae2f35a96b63fe131580fd059a1174ab0d294
Felipe Botero <FING156561@imac-de-andres.local>
2016-04-27 13:30:23 -0500

    Se agrega core-plot.

---
+++ MonitoringIOSProject/MonitoringIOSProject-Header-File.h
@@ -0,0 +1,14 @@
+//
+//  MonitoringIOSProject-Header-File.h
+//  MonitoringIOSProject
+//
+//  Created by FING156561 on 27/4/16.
+//  Copyright © 2016 FING156561. All rights reserved.
+//
+
+#ifndef MonitoringIOSProject_Header_File_h
+#define MonitoringIOSProject_Header_File_h
+
+
+#endif /* MonitoringIOSProject_Header_File_h */
+#import "CorePlot-CocoaTouch.h"
